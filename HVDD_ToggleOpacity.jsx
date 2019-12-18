6344dd2bca7cb041c6675cfd849f4f6bfb87e6b7
Hugo <hello@hugovdd.com>
2019-12-18 12:37:45 +0000

    cleaned up

--- HVDD_ToggleOpacity.jsx
+++ HVDD_ToggleOpacity.jsx
@@ -6,25 +6,6 @@ Website: http://hugovdd.com
 Description: Toggles opacity of selected layers. By dedault, if the layer's opacity is less or equals to 50, it'll set it to 100. and vice versa.
 */
 
-// function toggleOpacity() {
-//     app.beginUndoGroup("Change Opacity");
-//     var thisComp = app.project.activeItem; //Current compositon
-//     var selLayers = thisComp.selectedLayers; //Selected layers
-
-//     function setOpacity(layer, opacity) {
-//         layer.transform.opacity.setValue(opacity);
-//     }
-
-//     for (i = 0; i < selLayers.length; i++) {
-//         if (selLayers[i].transform.opacity.value <= 50) {
-//             setOpacity(selLayers[i], 100);
-//         } else {
-//             setOpacity(selLayers[i], 50);
-//         }
-//     }
-//     app.endUndoGroup();
-// }
-
 function toggleOpacityEffect() {
     app.beginUndoGroup("Change Opacity");
     var thisComp = app.project.activeItem; //Current compositon
@@ -43,37 +24,9 @@ function toggleOpacityEffect() {
         return false;
     }
 
-    // function hasAnyEffect(layer){
-    //     if (layer.Effects.numProperties != 0) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
-
-    // function hasOurEffect(layer){
-    //     if (hasAnyEffect(layer) == true){
-    //         hasTransformEffect(layer);
-    //     } else {
-    //         return false;
-    //     }
-    // }
-    // alert (hasTransformEffect(selLayers[0]));
-    // alert(hasOurEffect(selLayers[0]));
-    // alert(selLayers[0].Effects.numProperties);
-    // alert(selLayers[0].Effects.property(1).name);
-
     function removeTransformEffect(layer){ //works
         layer.Effects.property("HVDD_ToggleOpacity").remove();
     }
-    // selLayers[0].Effects.property("HVDD_ToggleOpacity").remove();
-    
-    // if (hasTransformEffect(selLayers[0])){
-    //     removeTransformEffect(selLayers[0]);
-    // } else {
-    //     setOpacityEffect(selLayers[0], 50);
-    // }
-
     
     for (i = 0; i < selLayers.length; i++) { //Loops through selected layers
         if (hasTransformEffect(selLayers[i]) == true) {
